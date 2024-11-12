ea95ba95aa332e77437cf60667ab1512839a1aca
JasonDinoPaws <87725873+JasonDinoPaws@users.noreply.github.com>
2024-11-11 22:48:06 -0700

    s

--- Client/Client.py
+++ Client/Client.py
@@ -1,16 +1,17 @@
 import socket
+import ClientUI
 
 client = socket.socket(socket.AF_BLUETOOTH, socket.SOCK_STREAM, socket.BTPROTO_RFCOMM)
 client.connect(("70:66:55:62:7a:9c", 4))
 
-
 try:
     while True:
-        client.send(input("EnterMessage:").encode())
         data = client.recv(1024)
         if not data:
             break
-        print(f"Message from server: {data.decode()}")
+
+        if data == "Coonect":
+            client.send(ClientUI.Connect())
 except OSError as e:
     pass
 
--- Client/ClientUI.py
+++ Client/ClientUI.py
@@ -2,18 +2,19 @@ from tkinter import *
 
 root = Tk()
 root.configure()
-root.attributes('-alpha',0)
+root.wm_attributes('-transparentcolor','#000000')
 root.attributes("-fullscreen",True)
 root.wm_title("event")
 
-Can = Canvas(root,width=root.winfo_screenwidth(),height=root.winfo_screenheight(),background="#000000", bd = 0.5, highlightthickness=0,)
+xSize = root.winfo_screenwidth()
+ySize = root.winfo_screenheight()
+
+Can = Canvas(root,width=xSize,height=ySize,background="#000000", highlightthickness=0)
 Can.pack()
 
 def Connect():
-    Can.config(bd=)
-    root.attributes('-alpha',1)
+    Can.create_rectangle(0,ySize/2-(.15*ySize),xSize,ySize/2+(.15*ySize),fill="white")
 
-Connect()
 try:
     while root.winfo_exists():
         root.update()
