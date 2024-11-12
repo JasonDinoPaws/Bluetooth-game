31512910967af511e8772fa1897a272c6af836e3
JasonDinoPaws <87725873+JasonDinoPaws@users.noreply.github.com>
2024-11-11 22:23:25 -0700

    cleint

--- Client/ClientUI.py
+++ Client/ClientUI.py
@@ -1,24 +1,24 @@
 from tkinter import *
 
-window = Tk()
-window.configure()
-window.wm_attributes('-transparentcolor','#000000')
-window.attributes("-fullscreen",True)
-window.wm_title("Game")
+root = Tk()
+root.configure()
+root.attributes('-alpha',0)
+root.attributes("-fullscreen",True)
+root.wm_title("event")
 
-c = Canvas(window,width=window.winfo_screenwidth(),bg='#000000',height=window.winfo_screenheight(),borderwidth=0,highlightthickness=0)
-c.pack()
-
-Cx = window.winfo_screenwidth()/2
-Cy = window.winfo_screenheight()/2
+Can = Canvas(root,width=root.winfo_screenwidth(),height=root.winfo_screenheight(),background="#000000", bd = 0.5, highlightthickness=0,)
+Can.pack()
 
 def Connect():
-    c.create_rectangle(Cx-50,Cy-50,Cx+50,Cy+50,fill="grey56", outline="")
+    Can.config(bd=)
+    root.attributes('-alpha',1)
 
+Connect()
 try:
-    while window.winfo_exists():
-        
-        window.update()
+    while root.winfo_exists():
+        root.update()
 
 except (EOFError,KeyboardInterrupt):
-    window.destroy()
+    pass
+
+root.destroy()
