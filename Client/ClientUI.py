49bd6d910ca062e1692d4dde7af3b3285d2ade72
JasonDinoPaws <87725873+JasonDinoPaws@users.noreply.github.com>
2024-11-18 14:43:35 -0700

    virus

--- Client/ClientUI.py
+++ Client/ClientUI.py
@@ -1,5 +1,6 @@
 from tkinter import *
 from PIL import Image, ImageTk
+from random import randint
 
 def CreateWindow():
     root = Tk()
@@ -12,42 +13,58 @@ def CreateWindow():
     
     Can = Canvas(root,width=xSize,height=ySize,background="#fcbd5d", highlightthickness=0)
 
+
     return root,Can,xSize,ySize
 
-def Create_Button(root:Tk, Canvas:Canvas, x1,y1,x2,y2, Text="Hello", FontSize = '15'):
+def Create_Button(root:Tk, Canvas:Canvas, x1,y1,x2,y2, Text="Hello", FontSize = '15',clicked = "<Button 1>"):
     x = root.winfo_pointerx()
     y = root.winfo_pointery()
     IsOver = (x >= x1 and x <= x2) and (y >= y1 and y <= y2)
 
     Canvas.create_rectangle(x1,y1,x2,y2,outline=IsOver and "White" or "gray",fill="")
     Canvas.create_text((x2+x1)/2, (y2+y1)/2,text=Text,font="Segoe "+FontSize,anchor="center",fill="White")
-    root.winfo_pointer
 
 
 
 def Connect(device="Fsih"):
     root, Can, xSize, ySize = CreateWindow()
     mess = StringVar()
-    Cy = ySize/2
-    Cx = xSize/2
+    x1 = .37*xSize
+    y1 = .3916*ySize
+    x2 = xSize-x1
+    y2 = ySize-y1
+    Fysize = y2-y1
+    Fxsize = x2-x1
+    
 
     def press(de):
         mess.set(de)
 
     Can.create_rectangle(0,0,xSize,ySize,fill="#000000",stipple="gray50")
-    Can.create_rectangle((0,Cy-(.15*ySize),xSize,Cy+(.15*ySize)),fill="#0f0f0f", outline="")
+    Can.create_rectangle(x1,y1,x2,y2,fill="#2B2B2B", outline="")
+    Can.create_rectangle(x1,y1+(.57264*Fysize),x2,y2,fill="#202020", outline="")
 
-    Can.create_text(Cx, Cy-(.075*ySize),text=f"Allow {device} to connect to this device",font= 'Segoe 35',anchor="s",fill="#ffffff")
-    Can.create_text(Cx, Cy-(.075*ySize),text=f"This could cause harm to your device",font= 'Segoe 25',anchor="n",fill="#ffffff")
-    
-    Can.pack()
+    Can.create_text(x1+(.03*Fxsize),y1+(.1709*Fysize),text=f"Allow {device} to connect to this device",font= 'Segoe 15',anchor="nw",fill="#ffffff")
+    Can.create_text(x1+(.03*Fxsize),y1+(.37179*Fysize),text=f"This could cause harm to your device",font= 'Segoe 10',anchor="nw",fill="#ffffff")
+
+    Can.pack() 
+    yesf = LabelFrame(root,highlightcolor="#ffffff",highlightthickness = 2, bd=0,background="#2B2B2B")
+    yesf.place(x=x1+(.07537*Fxsize),y=y2-(.320*Fysize),width=(.42211*Fxsize),height=(.2094*Fysize))
+    yes = Button(yesf,text="Yes",font="Segoe 15",command=lambda: press("yes"),background="#FA9A45",highlightcolor="#FA9A45")
+    yes.place(x=1,y=1,width=(.4070*Fxsize),height=(.1752*Fysize))
+
+    nof = LabelFrame(root,highlightcolor="#ffffff",highlightthickness = 2, bd=0,background="#2B2B2B")
+    nof.place(x=x2-(.4974*Fxsize),y=y2-(.320*Fysize),width=(.42211*Fxsize),height=(.2094*Fysize))
+    no = Button(nof,text="No",font="Segoe 15",command=lambda: press("no"),background="#FA9A45",highlightcolor="#FA9A45")
+    no.place(x=1,y=1,width=(.4070*Fxsize),height=(.1752*Fysize))
 
     while mess.get() not in ["yes","no"]:
-        Create_Button(root, Can, (.25*xSize),Cy+(.025*ySize),Cx-(.025*xSize),Cy+(.1*ySize),"Yes","25")
-        Create_Button(root, Can, Cx+(.025*xSize),Cy+(.025*ySize),Cx+(.25*xSize),Cy+(.1*ySize),"No","25")
         root.update()
 
     root.destroy()
     return mess.get()
 
-print(Connect())
+images = ["Lucky.png","melancholu.png","Wana.png"]
+def Virus():
+    root, Can, xSize, ySize = CreateWindow()
+
