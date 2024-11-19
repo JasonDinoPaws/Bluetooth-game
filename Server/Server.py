b18b33f95cc8278a3960297ece366e24b3b898c4
JasonDinoPaws <87725873+JasonDinoPaws@users.noreply.github.com>
2024-11-19 07:41:22 -0700

    sever

--- Server/Server.py
+++ Server/Server.py
@@ -1,19 +1,24 @@
 import socket
 
+allowed = 3
+connected = []
 server = socket.socket(socket.AF_BLUETOOTH, socket.SOCK_STREAM, socket.BTPROTO_RFCOMM)
 server.bind(("70:66:55:62:7a:9c", 4))
-server.listen(1)
+server.listen(allowed)
 
-client, addr = server.accept()
+while len(connected) < allowed:
+    client, addr = server.accept()
+    connected.append([client,addr])
+    print(addr,"Has connected")
 
-print("Connected")
+def SendToClients(clients,mess:str):
+    for cl,ad in clients:
+        cl.send(mess.encode())
+
 try:
     while True:
-        client.send(input("Send Message: ").encode())
-        data = client.recv(1024)
-        if not data:
-            break
-        print(f"Message from {addr}: {data.decode()}")
+        SendToClients(connected,input("Send Message: "))
+
 except OSError as e:
     pass
 
