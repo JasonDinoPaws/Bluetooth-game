3ea2b7b04fe55c25b607c7c61b3890804cc56986
JasonDinoPaws <87725873+JasonDinoPaws@users.noreply.github.com>
2024-11-19 10:21:08 -0700

    sevr

--- Server/Server.py
+++ Server/Server.py
@@ -1,19 +1,21 @@
 import socket
+import ServerUi
 
-allowed = 2
 connected = []
 server = socket.socket(socket.AF_BLUETOOTH, socket.SOCK_STREAM, socket.BTPROTO_RFCOMM)
 server.bind(("70:66:55:62:7a:9c", 4))
-server.listen(allowed)
+server.listen(1)
 
-while len(connected) < allowed:
+ServerUi.viewcleints(connected)
+while not ServerUi.start:
+    ServerUi.viewcleints(connected)
     client, addr = server.accept()
     hn = client.recv(1024)
-    connected.append([client,addr,hn.decode()])
-    print(hn.decode(),"Has connected with address",addr)
+    connected.append([client,addr,hn.decode(),False])
+    print(hn.decode(),"Has connected with address",addr[0])
 
 def SendToClients(clients,mess:str):
-    for cl,ad in clients:
+    for cl,ad,hn,alo in clients:
         cl.send(mess.encode())
 
 try:
--- Server/ServerUi.py
+++ Server/ServerUi.py
@@ -1,10 +1,26 @@
 from tkinter import *
+start = False
 
 window = Tk()
 window.configure()
 window.attributes("-fullscreen",True)
 window.wm_title("Game")
+xSize = window.winfo_screenwidth()
+ySize = window.winfo_screenheight()
 
-c = Canvas(window,width=window.winfo_screenwidth(),bg='#36342f',height=window.winfo_screenheight(),borderwidth=0,highlightthickness=0)
+c = Canvas(window,width=xSize,bg='#000000',height=ySize,borderwidth=0,highlightthickness=0)
 c.pack()
 
+def viewcleints(clients):
+    window.update()
+    c.delete("all")
+
+    x1 = .1*xSize
+    x2 = xSize-x1
+    y1 = .05*ySize
+    y2 = .15*ySize
+    for _,add,hn,alo in clients:
+        c.create_rectangle(x1,y1,x2,y2,fill="gray")
+        c.create_text(x1+(.01*xSize),y1,text=hn,anchor="nw")
+        c.create_text(x2+(.01*xSize),y1,text="addr",anchor="ne")
+    window.update()
