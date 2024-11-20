f0a3b29a2382524c3189deeba030b8e36416e7ef
JasonDinoPaws <87725873+JasonDinoPaws@users.noreply.github.com>
2024-11-20 15:06:29 -0700

    server

--- Server/Server.py
+++ Server/Server.py
@@ -1,25 +1,27 @@
 import socket
-import ServerUi
+#import ServerUi
 
 connected = []
 server = socket.socket(socket.AF_BLUETOOTH, socket.SOCK_STREAM, socket.BTPROTO_RFCOMM)
 server.bind(("70:66:55:62:7a:9c", 4))
 server.listen(1)
 
-ServerUi.viewcleints(connected)
-while not ServerUi.start:
-    ServerUi.viewcleints(connected)
+#print("Socket Open")
+
+#ServerUi.viewcleints(connected)
+while len(connected) < 5:#not ServerUi.Start and ServerUi.window.winfo_exists():
     client, addr = server.accept()
     hn = client.recv(1024)
     connected.append([client,addr,hn.decode(),False])
     print(hn.decode(),"Has connected with address",addr[0])
+    #ServerUi.viewcleints(connected)
 
 def SendToClients(clients,mess:str):
-    for cl,ad,hn,alo in clients:
+    for cl,_,_,_ in clients:
         cl.send(mess.encode())
 
 try:
-    while True:
+    while True:#ServerUi.window.winfo_exists():
         SendToClients(connected,input("Send Message: "))
 
 except OSError as e:
--- Server/ServerUi.py
+++ Server/ServerUi.py
@@ -1,5 +1,6 @@
 from tkinter import *
-start = False
+import threading
+Start = False
 
 window = Tk()
 window.configure()
@@ -11,16 +12,23 @@ ySize = window.winfo_screenheight()
 c = Canvas(window,width=xSize,bg='#000000',height=ySize,borderwidth=0,highlightthickness=0)
 c.pack()
 
-def viewcleints(clients):
-    window.update()
+exit = Button(window,text="X",background="red",command=lambda: window.destroy())
+exit.place(width=.025*xSize,height=.05*ySize,x=5,y=5)
+
+def viewcleints(clients=[]):
     c.delete("all")
 
     x1 = .1*xSize
     x2 = xSize-x1
     y1 = .05*ySize
-    y2 = .15*ySize
+    y2 = .1*ySize
+
     for _,add,hn,alo in clients:
         c.create_rectangle(x1,y1,x2,y2,fill="gray")
-        c.create_text(x1+(.01*xSize),y1,text=hn,anchor="nw")
-        c.create_text(x2+(.01*xSize),y1,text="addr",anchor="ne")
+        c.create_text(x1+(.01*xSize),(y1+y2)/2,text=hn,anchor="w",font= 'Segoe 15')
+        c.create_text((x2+x1)/2,(y1+y2)/2,text=add[0],anchor="center",font= 'Segoe 15')
+        c.create_text(x2-(.01*xSize),(y1+y2)/2,text=alo,anchor="e",font= 'Segoe 15')
+        y1 += y2/1.5
+        y2 += y2/1.5
+
     window.update()
